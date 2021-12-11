cdbc736b19a4254833942fd257abe7d660e07c5a
jls83 <josephlsanders@gmail.com>
2021-12-10 22:52:35 -0500

    Change d3p1 impl to functional style

--- day_3/src/part_1.rs
+++ day_3/src/part_1.rs
@@ -1,30 +1,35 @@
+use std::cmp::Ordering;
 use std::include_str;
 
+fn thing(b: i32) -> impl Fn(&Vec<i32>) -> (i32, i32) {
+    move |ns: &Vec<i32>| -> (i32, i32) {
+        let shifted = 2i32.pow(b.try_into().unwrap());
+        let n: i32 = ns.iter()
+            .map(|n| (n & shifted) >> b)
+            .sum();
+        match (n * 2).partial_cmp(&(ns.len() as i32)).unwrap() {
+            Ordering::Greater => (shifted, 0),
+            Ordering::Less => (0, shifted),
+            Ordering::Equal => (0, 0),
+        }
+    }
+}
 
 fn main() {
-    let bits = 12;
-
     let ns: Vec<i32> = include_str!("../input.txt")
         .lines()
         .map(|line| i32::from_str_radix(line, 2).unwrap())
         .collect();
 
-    let mut delta: i32 = 0;
-    let mut epsilon: i32 = 0;
+    let bits: i32 = 12;
 
-    for b in 0..bits {
-        let thing = 2i32.pow(b);
-        let foo: i32 = ns.iter()
-            .map(|n| (n & thing) >> b)
-            .sum();
+    let bar = (0..bits)
+        .map(thing)
+        .map(|f| f(&ns))
+        .fold((0, 0), |acc, cur| {
+            (acc.0 + cur.0, acc.1 + cur.1)
+        });
 
-        if foo > (ns.len() / 2).try_into().unwrap() {
-            delta |= thing;
-        } else {
-            epsilon |= thing;
-        }
-    }
-
-    println!("{}", delta * epsilon);
+    println!("{}", bar.0 * bar.1);
 
 }
