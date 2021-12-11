7695a52773be331105ae6b128ee6e649fc2c9fb7
jls83 <josephlsanders@gmail.com>
2021-12-11 13:56:46 -0500

    Add impl for d3p2

--- day_3/src/part_2.rs
+++ day_3/src/part_2.rs
@@ -1,34 +1,30 @@
 use std::include_str;
-use std::collections::HashMap;
 
-struct Node<T> {
-    char: T,
-    children: HashMap<i8, Vec<Box<Node<T>>>>,
-    sub_items: u32,
-}
+fn get_most_least_common(ns: &Vec<i32>, bit: u32) -> (Vec<i32>, Vec<i32>) {
+    let mut ones: Vec<i32> = Vec::new();
+    let mut zeros: Vec<i32> = Vec::new();
 
-struct Thing<T> {
-    root: Node<T>,
-}
+    let thing = 2i32.pow(bit);
 
-impl<String> Thing<String> {
-    fn insert(&mut self, item: String) {
-        let mut d = item.len(); // digit "length"
-        let mut current = self.root;
-
-        for char in item.chars() {
-            current.sub_items += d;
-            if None(current.children.get(&char)) {
-                current.children.insert(char, Node<String> { char: char })
-                set_current_dir.c
-
-
-            }
-
+    for n in ns {
+        if (n & thing) == 0 {
+            zeros.push(*n);
+        } else {
+            ones.push(*n);
         }
     }
-}
 
+    // More zeros than ones
+    if zeros.len() > ones.len() {
+        (zeros, ones)
+    }
+    // More ones than zeros
+    // Equal number, ones are the "most common" by default
+    else { // if ones.len() >= zeros.len() {
+        (ones, zeros)
+    }
+
+}
 
 fn main() {
     let bits = 12;
@@ -38,22 +34,19 @@ fn main() {
         .map(|line| i32::from_str_radix(line, 2).unwrap())
         .collect();
 
-    let mut delta: i32 = 0;
-    let mut epsilon: i32 = 0;
+    let mut current_bit = bits - 1;
+    let (mut most_common, mut least_common) = get_most_least_common(&ns, current_bit);
 
-    for b in 0..bits {
-        let thing = 2i32.pow(b);
-        let foo: i32 = ns.iter()
-            .map(|n| (n & thing) >> b)
-            .sum();
-
-        if foo > (ns.len() / 2).try_into().unwrap() {
-            delta |= thing;
-        } else {
-            epsilon |= thing;
-        }
+    while most_common.len() != 1 {
+        current_bit -= 1;
+        most_common = get_most_least_common(&most_common, current_bit).0;
     }
 
-    println!("{}", delta * epsilon);
+    let mut current_bit = bits - 1;
+    while least_common.len() != 1 {
+        current_bit -= 1;
+        least_common = get_most_least_common(&least_common, current_bit).1;
+    }
 
+    println!("{}", most_common[0] * least_common[0]);
 }
