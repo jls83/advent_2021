e06bf751cc3ee800ca8478a76fb9d9e8a8ef4b04
jls83 <josephlsanders@gmail.com>
2021-12-16 14:22:05 -0500

    Add parsing logic for day 8 in Rust

--- day_8/src/part_1.rs
+++ day_8/src/part_1.rs
@@ -1,91 +1,84 @@
 use std::include_str;
 use std::collections::{HashMap, HashSet};
 
-#[derive(Default)]
-struct Display {
-    lights: HashMap<u32, bool>,
+fn build_set(items: &Vec<HashSet<char>>, all_pins_set: &HashSet<char>) -> HashSet<char> {
+    items.into_iter()
+        .map(|item| all_pins_set - &item)
+        .fold(
+            HashSet::new(),
+            |a, c| {
+                a.union(&c).cloned().collect()
+            })
 }
 
-impl Display {
-    fn possible_pins(&self) -> HashMap<u32, HashSet<u32>> {
-        let mut res: HashMap<u32, HashSet<u32>> = HashMap::new();
-
-        let possible_pins = match self.lights.keys().count() {
-            2 => vec![2, 5],
-            3 => vec![0, 2, 5],
-            4 => vec![1, 2, 3, 5],
-            5 => vec![0, 1, 2, 3, 4, 5, 6],
-            6 => vec![0, 1, 2, 3, 4, 5, 6],
-            7 => vec![0, 1, 2, 3, 4, 5, 6],
-            _ => vec![],
-        }.into_iter().collect::<HashSet<u32>>();
-
-        for pin in self.lights.keys() {
-            let val = res.entry(*pin).or_insert(HashSet::new());
-            val.extend(possible_pins.clone());
-        }
-
-        res
-    }
+fn build_map_key(pin_coll: &HashSet<char>) -> String {
+    let mut blah: Vec<&char> = pin_coll.into_iter().collect();
+    blah.sort();
+    blah.iter().map(|c| *c).collect()
 }
 
-#[derive(Default)]
-struct Configuration {
-    patterns: Vec<Display>,
-    output: Vec<Display>,
-    translation: HashMap::<u32, u32>,
-}
-
-impl Configuration {
-    fn foo(&self) -> HashMap<u32, HashSet<u32>> {
-        let mut res: HashMap<u32, HashSet<u32>> = HashMap::new();
-        for pattern in &self.patterns {
-            for (pin, possible_pins) in pattern.possible_pins() {
-                let val = res.entry(pin).or_insert(HashSet::new());
-                val.extend(possible_pins.clone());
-            }
-        }
-
-        res
-    }
-}
-
-fn parse_pattern(pattern_str: &str) -> Display {
-    let lights: HashMap<u32, bool> = pattern_str.chars()
-        .map(|c| ((c as u32) - 97, true))
-        .collect();
-    Display { lights }
-}
-
-fn parse_line(line: &str) -> Result<Configuration, &'static str> {
-    let foo: Vec<Vec<&str>> = line.split(" | ")
-        .map(|segment: &str| {
-            segment.split(" ").collect()
-        })
-        .collect();
-
-    match &foo[..] {
-        [pattern_strs, output_strs] => {
-            let patterns: Vec<Display> = pattern_strs.iter()
-                .map(|p| parse_pattern(p))
-                .collect();
-            let output: Vec<Display> = output_strs.iter()
-                .map(|p| parse_pattern(p))
-                .collect();
-            Ok(Configuration { patterns, output, translation: HashMap::new() }
-        )},
-        _ => Err("Whoops"),
+fn get_pin_mapping(things: Vec<&str>) -> HashMap<String, i32> {
+    let mut len_map: HashMap<usize, Vec<HashSet<char>>> = HashMap::new();
+    for thing in things {
+        let val = len_map.entry(thing.len()).or_insert(Vec::new());
+        val.push(thing.chars().collect());
     }
 
+    let size_2_set = &len_map[&2][0];
+    let size_3_set = &len_map[&3][0];
+    let size_4_set = &len_map[&4][0];
+    let size_7_set = &len_map[&7][0];
+
+    let size_5_items = &len_map[&5];
+    let size_6_items = &len_map[&6];
+
+    let size_5_diff_pins = build_set(size_5_items, size_7_set);
+    let size_6_diff_pins = build_set(size_6_items, size_7_set);
+
+    let set_b_d = size_4_set - size_2_set;
+    let set_d = &set_b_d.intersection(&size_6_diff_pins).copied().collect();
+    let set_b = &set_b_d - &set_d;
+
+    let p_0 = size_7_set - set_d;
+
+    let set_c_e = size_6_diff_pins.intersection(&p_0).copied().collect();
+    let set_c = size_2_set.intersection(&set_c_e).copied().collect();
+    let set_e = &set_c_e - &set_c;
+    let set_b_e: HashSet<char> = set_b.union(&set_e).cloned().collect();
+    let set_b_c_e: HashSet<char> = set_c_e.union(&set_b).cloned().collect();
+    let set_f = &size_5_diff_pins - &set_b_c_e;
+    let set_b_f = set_b.union(&set_f).cloned().collect();
+
+    let p_2 = size_7_set - &set_b_f;
+    let p_3 = size_7_set - &set_b_e;
+    let p_5 = size_7_set - &set_c_e;
+    let p_6 = size_7_set - &set_c;
+    let p_9 = size_7_set - &set_e;
+
+    vec![
+        (build_map_key(&p_0), 0),
+        (build_map_key(size_2_set), 1),  // p_1
+        (build_map_key(&p_2), 2),
+        (build_map_key(&p_3), 3),
+        (build_map_key(size_4_set), 4),  // p_4
+        (build_map_key(&p_5), 5),
+        (build_map_key(&p_6), 6),
+        (build_map_key(size_3_set), 7), // p_7
+        (build_map_key(size_7_set), 8), // p_8
+        (build_map_key(&p_9), 9),
+    ].into_iter().collect()
 }
 
 fn main() {
-    let configs = include_str!("../example.txt")
+    let configs: Vec<Vec<&str>> = include_str!("../example.txt")
         .lines()
-        .map(parse_line)
-        .map(Result::unwrap);
+        .map(|line| {
+            line.split(" | ").collect::<Vec<&str>>()[0]
+        })
+        .map(|s| s.split(" ").collect::<Vec<&str>>())
+        .collect();
 
     for config in configs {
-        println!("{:?}", config.foo());
+        println!("{:?}", get_pin_mapping(config));
     }
 }
