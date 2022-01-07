d5f45834957b537b6b8779f4e413cc4ca8457748
jls83 <josephlsanders@gmail.com>
2022-01-07 16:48:08 -0500

    Add d17 sketch, infra

---
+++ day_17/sketch.py
@@ -0,0 +1,7 @@
+def triangular_n_between(a, b):
+    x = 0
+
+    while not (a <= ((x * (x - 1)) / 2) <= b):
+        x += 1
+
+    return (x * (x - 1)) // 2
---
+++ day_17/src/part_1.rs
@@ -0,0 +1,7 @@
+use std::include_str;
+
+fn main() {
+    let lines = include_str!("../input.txt").lines();
+    // let lines = include_str!("../example.txt").lines();
+
+}
