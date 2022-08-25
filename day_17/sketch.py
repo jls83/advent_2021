99b4cff8ae6dd867923f2cefa9f52620ffb0235b
jls83 <josephlsanders@gmail.com>
2022-08-25 13:00:52 -0400

    Add further sketches

--- day_17/sketch.py
+++ day_17/sketch.py
@@ -1,3 +1,4 @@
+import math
 from typing import Tuple
 
 
@@ -60,6 +61,68 @@ def next_position(curr_position: Point, curr_velocity: VelocityVector) -> Tuple[
     next_y_position, next_y_velocity = next_y_position_and_velocity(curr_position, curr_velocity)
 
     return Point(next_x_position, next_y_position), VelocityVector(next_x_velocity, next_y_velocity)
+
+
+######
+
+def get_min_velocity(measure: int) -> int:
+    return math.floor(math.sqrt(measure * 2))
+
+
+def get_max_time(measure: int, velocity: int) -> int:
+    t = 0
+    while measure > 0:
+        measure -= velocity
+        velocity -= 1
+        t += 1
+
+    return t
+
+def pos_y(initial_velocity: int, t: int, initial_position: int = 0) -> int:
+    gravity_sum = (t * (t - 1)) // 2  # TODO: comment
+    return initial_position + (initial_velocity * t) - gravity_sum
+
+def pos_x(initial_velocity: int, t: int, initial_position: int = 0) -> int:
+    is_negative = (initial_velocity < 0)
+    velocity = abs(initial_velocity)
+
+    distance_travelled = 0
+    while t > 0 and velocity > 0:
+        distance_travelled += velocity
+        velocity -= 1
+        t -= 1
+
+    if is_negative:
+        distance_travelled *= -1
+
+    return distance_travelled + initial_position
+
+
+def foo(initial_velocity: int, position: int):
+    b = ((2 * initial_velocity) + 1) / 2
+    discrim = math.sqrt((b ** 2) + (2 ** position))
+    return b+discrim, b-discrim
+
+def x_velo_in_range(velocity: int, start: int, end: int):
+    t = 0
+    position = 0
+    res = []
+    while position < end:
+        if start <= position <= end:
+            res.append(t)
+        next_position = position + velocity
+        if next_position == position:
+            return res, True
+        position = next_position
+        velocity -= 1
+        t += 1
+
+    return res, False
+
+def get_valid_x_velos(min_velocity: int, start: int, end: int):
+    blah = ((i, x_velo_in_range(i, start, end)) for i in range(min_velocity, end))
+    d = {i: velos for i, velos in blah if velos[0]}
+    return d
 
 
 def run_trial(target_rectangle: Rectangle, initial_velocity: VelocityVector) -> Tuple[bool, int]:
